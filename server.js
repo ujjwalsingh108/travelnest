4eeea05307066e9bef6c77549670c9133b9d200a
ujjwal-ampcome <ujjwal.singh@ampcome.com>
2024-04-09 15:26:24 +0530

    added libraries

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
 // ENTRY POINT OF OUR APPLICATION
+const mongoose = require('mongoose');
+
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
-// console.log(process.env);
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection successfull'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
 
+// console.log(process.env);
 /* console.log(app.get('env')); 
 CHECKING THE ENV IN WHICH WE ARE WORKING
 */
